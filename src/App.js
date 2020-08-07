23c60251dca27cf5a4450c2c076309f84addd41c
Sean <contact@seanyager.com>
2020-08-06 20:48:22 -0500

    React Beer App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import BeerList from './components/BeerList';
+import './styles/BeerList.css'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      data:[]
+    }
+  }
+
+    componentDidMount() {
+      fetch('https://api.punkapi.com/v2/beers')
+        .then(res => res.json())
+        .then(examples => {
+            this.setState({ data: examples })
+            console.log(examples)
+        })
+    }
+
+    render() {
+        return (
+            <div>
+              <ul>
+                { 
+                  this.state.data.map((beer, index) => {
+                    return <BeerList key={index} beerId={beer.id} beerName={beer.name} beerTag={beer.tagline} beerImg={beer.image_url}/>
+                  })
+                }
+              </ul>
+            </div>
+        )
+    }
 }
 
 export default App;
